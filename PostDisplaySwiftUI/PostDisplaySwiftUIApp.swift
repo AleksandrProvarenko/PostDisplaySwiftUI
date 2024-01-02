9bcac8a6a5a5d11c5ee18ac2e1d3287536f8dcfa
Aleksandr Provarenko <provar67@gmail.com>
2024-01-02 09:49:55 -0500

    Initil commit

---
+++ PostDisplaySwiftUI/PostDisplaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostDisplaySwiftUIApp.swift
+//  PostDisplaySwiftUI
+//
+//  Created by Alex Provarenko on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PostDisplaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
